9d050cab45cf225474cb1645fd958a4c91753665
Selim albert <selim.b33@gmail.com>
2018-08-01 18:52:20 +0200

    first commit in a while, bunch of stuff, haml gem

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'simple_form'
 gem 'bootstrap-sass'
 gem 'devise-i18n'
+gem 'haml'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/product_list_controller.rb
+++ app/controllers/product_list_controller.rb
@@ -1,25 +1,40 @@
 class ProductListController < ApplicationController
 
   def index
-    @product_list = Product_list.all 
+    @product_list = Product_list.all
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 
   def new
+    @product = Product.new
   end
 
   def create
-  end 
+    @product = Product.new(product_params)
+    if product.save
+      redirect_to product_list
+    else
+      render :new
+    end
+  end
 
   def edit
+    @product = Product.find(params[:id])
   end
 
   def update
+
   end
 
   def destroy
+  end
+
+  private
+
+  def product_params
   end
 
 end
